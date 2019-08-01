e1d481c68aae94af40fcd80234a50d8358928110
Sarah Franc <sfranc@student.42.fr>
2019-08-01 15:01:11 +0200

    feat(server): init

---
+++ ftp_server/incs/server.h
@@ -0,0 +1,13 @@
+#ifndef SERVER_H
+# define SERVER_H
+
+# include "libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/socket.h>
+# include <netdb.h>
+
+# define NB_CONNECT		42
+# define BUF_SIZE		1024
+
+#endif
