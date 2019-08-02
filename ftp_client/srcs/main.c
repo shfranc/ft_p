bb2d3966083ac5cee2a398984d84d6df2b5c8c5d
Sarah Franc <sfranc@student.42.fr>
2019-08-02 11:03:33 +0200

    feat(client): add localhost as accepted addr

--- ftp_client/srcs/main.c
+++ ftp_client/srcs/main.c
@@ -12,6 +12,13 @@ void	usage(char *prog_name)
 	exit(1);
 }
 
+int		get_inet_addr(char *addr)
+{
+	if (ft_strcmp(addr, "localhost") == 0)
+		return (INADDR_ANY);
+	else return (inet_addr(addr));
+}
+
 int		create_client(char *addr, int port)
 {
 	int						client_sock;
@@ -25,7 +32,7 @@ int		create_client(char *addr, int port)
 		return (ret_error("socket: error"));
 	client_sin.sin_family = AF_INET;
 	client_sin.sin_port = htons(port);
-	client_sin.sin_addr.s_addr = inet_addr(addr);
+	client_sin.sin_addr.s_addr = get_inet_addr(addr);
 	if (connect(client_sock, (const struct sockaddr *)&client_sin, sizeof(client_sin)))
 		return (ret_error("connect: error"));
 	return (client_sock);
