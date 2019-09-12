a8824f1f9401b6959912170f92d9237bee1e8b5b
Sarah Franc <sfranc@student.42.fr>
2019-09-12 15:12:50 +0200

    feat(server): ipv6 - epsv done

--- ftp_server/srcs/cmd_epsv.c
+++ ftp_server/srcs/cmd_epsv.c
@@ -43,12 +43,16 @@ void		cmd_epsv(t_user *user, char **cmd)
 	struct sockaddr_in	data_sin;
 	unsigned int		data_sin_len;
 
-	(void)cmd;
-	log_info("Passive mode");
+	if (ft_tablen(cmd) > 1)
+	{
+		close_data_channel(user);
+		return (send_to_user_ctrl(user, RESP_501));
+	}
+	log_info("Extended passive mode");
 	if ((ret = create_DTP_server(user)) == -1)
 		return (send_to_user_ctrl(user, RESP_425));
 	message = NULL;
-	asprintf(&message, "227 Data channel open (0,0,0,0,%d,%d)", (unsigned char)(user->dtp_port >> 8), (unsigned char)user->dtp_port);
+	asprintf(&message, "229 Entering extended passive mode (|||%d|)", user->dtp_port);
 	send_to_user_ctrl(user, message);
 	if ((user->data_sock = accept(user->server_dtp_sock,
 		(struct sockaddr *)&data_sin, &data_sin_len)) < 0)
--- misc/TODO.md
+++ misc/TODO.md
@@ -1,29 +1,29 @@
 # TODO
 
 ## Server
-√ pasv  
-√ list:  
-	- protect path  
-	- symlink  
-√ port  
-√ retr:  
-	√ type  
-√ pwd  
-- cd:  
-	- protect path  
-	- symlink  
-- replace asprintf in cmd_pasv  
-- stor  
+√ pasv
+√ list:
+	- protect path
+	- symlink
+√ port
+√ retr:
+	√ type
+√ pwd
+- cd:
+	- protect path
+	- symlink
+- replace asprintf in cmd_pasv
+- stor
 - refaire mieux les logs
 - close_server avec pas de t_ex_ret en param
 
 ## Client
-- ls  
-- cd  
-- get  
-- put  
-- pwd  
-- quit  
+- ls
+- cd
+- get
+- put
+- pwd
+- quit
 
 ## Bonus
 √ RFC
@@ -34,3 +34,9 @@
 	- logs client
 	- prompt client avec le path
 	- basic user/pass authent
+
+## Stuff
+- asprintf PASV + EPSV
+- protect ls path
+- potect get path
+- get ascii
