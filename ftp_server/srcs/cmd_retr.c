d7c7f41157ae04f0b23d4ca699e66a11f9e03aaa
Sarah Franc <shfranc@gmail.com>
2019-09-20 15:11:37 +0200

    fix(cmd_retr): add is valid path

--- ftp_server/srcs/cmd_retr.c
+++ ftp_server/srcs/cmd_retr.c
@@ -18,12 +18,31 @@ static void				send_file(t_user *user, const void *ptr, size_t size)
 	send_to_user_ctrl(user, RESP_226);
 }
 
+static t_bool			check_file_path(t_user *user, char *filename)
+{
+	char		*virtual_path;
+
+	logger(LOG_INFO, "Trying to fetch", filename);
+	virtual_path = convert_path_real_to_virtual(filename);
+	if (is_valid_path(virtual_path) == FALSE)
+	{
+		free(virtual_path);
+		logger(LOG_INFO, "Invalid path, no get performed.", NULL);
+		send_to_user_ctrl(user, RESP_550_1);
+		return (FALSE);
+	}
+	free(virtual_path);
+	return (TRUE);
+}
+
 static void 			get_file(t_user *user, char *filename)
 {
 	int			fd;
 	struct stat buf;
 	void		*ptr;
 
+	if (check_file_path(user, filename) == FALSE)
+		return ;
 	fd = open(filename, O_RDONLY);
 	if (fd < 0 || fstat(fd, &buf) < 0)
 		return(send_to_user_ctrl(user, RESP_550));
@@ -41,6 +60,9 @@ static void 			get_file(t_user *user, char *filename)
 
 void				cmd_retr(t_user *user, char **cmd)
 {
+	char 	*abs_virtual_path;
+	char 	*real_path;
+
 	if (ft_tablen(cmd) != 2)
 	{
 		close_data_channel(user);
@@ -51,6 +73,10 @@ void				cmd_retr(t_user *user, char **cmd)
 	else
 		send_to_user_ctrl(user, RESP_125);
 	logger(LOG_INFO, "Fetching", cmd[1]);
-	get_file(user, cmd[1]);
+	abs_virtual_path = get_virtual_absolute_path(user, cmd[1]);
+	real_path = convert_path_virtual_to_real(abs_virtual_path);
+	get_file(user, real_path);
+	free(abs_virtual_path);
+	free(real_path);
 	close_data_channel(user);
 }
