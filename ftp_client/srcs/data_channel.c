bdb251734a6c9927b664af659956eca79f85c1b8
Sarah Franc <shfranc@gmail.com>
2019-09-29 23:40:24 +0200

    feat(client): data channel - mode auto

--- ftp_client/srcs/data_channel.c
+++ ftp_client/srcs/data_channel.c
@@ -14,11 +14,12 @@ void		open_data_channel()
 {
 	if (g_client.pass == ON)
 		return (connect_data_channel());
+	if (g_client.pass == OFF)
+		return (create_data_channel());
 	else if (g_client.pass == AUTO)
 	{
-		if (connect_data_channel() == FALSE)
-			return (create_data_channel);
+		connect_data_channel();
+		if (g_client.data_sock == -1)
+			return (create_data_channel());
 	}
-	else if (g_client.pass == OFF)
-		return (create_data_channel());
 }
