b38c4c57c4bf811fb25a6b25e30731c0e07593ff
Sarah Franc <sfranc@student.42.fr>
2019-12-06 16:26:53 +0100

    feat(epsv): use generic function to connect to the DTP server

--- ftp_client/srcs/cmd_epsv.c
+++ ftp_client/srcs/cmd_epsv.c
@@ -15,17 +15,6 @@ static char		**parse_response_epsv(char *msg)
 	return (details);
 }
 
-static int			connect_to_DTP_server(uint16_t port)
-{
-	int						data_sock;
-
-	if (!(data_sock = create_socket(g_client.family)))
-		return (-1);
-	if (connect_to_server_sock(data_sock, g_client.addr, port) != 0)
-		return (-1);
-	return (data_sock);
-}
-
 void					cmd_epsv()
 {
 	char		**details;
@@ -40,7 +29,7 @@ void					cmd_epsv()
 	g_client.data_port = ft_atoi(details[0]);
 	log_info_msg("addr", g_client.addr);
 	log_info_nb("port", g_client.data_port);
-	if ((g_client.data_sock = connect_to_DTP_server(g_client.data_port)) == -1)
+	if ((g_client.data_sock = connect_to_server(g_client.addr, g_client.data_port)) == -1)
 		return (log_error("Failed to connect to the data channel."));
 	ft_freetab(&details);
 }
